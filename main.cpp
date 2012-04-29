118ced59f2fdf0ef87b469cebeb3c15e1e453c31
Martin C. Richards <martinffx@gmail.com>
2012-04-29 14:02:44 +0200

    Finished letters

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@
 #include <GL/glut.h>
 #include <math.h>
 #include <stdio.h>
+#include "3DCurve.h"
 
 //======================================================
 // GLOBAL VARIABLES & FUNCTIONS
@@ -33,79 +34,6 @@ float pitch0, yaw0;
 bool MousePressed;
 int mouseX0, mouseY0;
 bool rotating=false;
-
-void draw3Dcurve(double depth, double r1, double r2, double theta_start, double theta_stop, double theta_inc) {
-	// Function to draw 3D curve 
-	// depth = depth centred round z=0
-	// r1 = inner radius
-	// r2 = outer radius
-	// theta_start = start angle in degrees measured from x-axis
-	// theta_stop = similar 
-
-	double x, y, x1, x2, y1, y2, z, thet, z_front, z_back;
-	int i=0;
-	double radius=1.5, c=3.14159/180.0;
-	z_front=depth/2; z_back=-depth/2;
-	
-	// draw rear face (away from viewer)
-	glColor3f(0.0, 0.0, 0.0);
-	z=z_back;
-	glBegin(GL_QUAD_STRIP);
-	for(thet=theta_start; thet<=theta_stop;thet+=theta_inc) {
-		x=cos(c*thet)*r2; y=sin(c*thet)*r2; glVertex3d(x,y,z);
-		x=cos(c*thet)*r1; y=sin(c*thet)*r1; glVertex3d(x,y,z);
-	}
-	glEnd();
-
-	// draw front face (closer to viewer)
-	glColor3f(0.0, 0.0, 0.0);
-	z=z_front;
-	glBegin(GL_QUAD_STRIP);
-	for(thet=theta_start; thet<=theta_stop;thet+=theta_inc)	{
-		x=cos(c*thet)*r1; y=sin(c*thet)*r1; glVertex3d(x,y,z);
-		x=cos(c*thet)*r2; y=sin(c*thet)*r2; glVertex3d(x,y,z);
-	}
-	glEnd();
-
-	// draw upper face
-	glColor3f(0.0, 0.0, 0.0);
-	glBegin(GL_QUAD_STRIP);
-	for(thet=theta_start; thet<=theta_stop;thet+=theta_inc) {
-		x=cos(c*thet)*r2; y=sin(c*thet)*r2;
-		z=z_front; glVertex3d(x,y,z);
-		z=z_back;  glVertex3d(x,y,z);
-	}
-	glEnd();
-
-	// draw lower face
-	glBegin(GL_QUAD_STRIP);
-	for(thet=theta_start; thet<=theta_stop;thet+=theta_inc)	{
-		x=cos(c*thet)*r1; y=sin(c*thet)*r1;
-		z=z_back; glVertex3d(x,y,z);
-		z=z_front; glVertex3d(x,y,z);
-	}
-	glEnd();
-
-	// draw bottom end
-	glColor3f(0.0, 0.0, 0.0);
-	glBegin(GL_POLYGON);
-		glVertex3d(r1,0.0,z_front);
-		glVertex3d(r1,0.0,z_back);
-		glVertex3d(r2,0.0,z_back);
-		glVertex3d(r2,0.0,z_front);
-	glEnd();
-
-	// draw top end
-	glBegin(GL_POLYGON);
-		x1=cos(c*theta_stop)*r1; y1=sin(c*theta_stop)*r1;
-		x2=cos(c*theta_stop)*r2; y2=sin(c*theta_stop)*r2;
-
-		glVertex3d(x1,y1,z_front);
-		glVertex3d(x2,y2,z_front);
-		glVertex3d(x2,y2,z_back);
-		glVertex3d(x1,y1,z_back);
-	glEnd();
-}
 
 //======================================================
 // DRAW M
@@ -287,15 +215,10 @@ void drawRightTrunk(){
 }
 
 void drawM(void) {
-	glPushMatrix();
-    glTranslatef(-2, 0, 0);
-     
 	drawRightTrunk();
 	drawLeftTrunk();
 	drawLeftBranch();
 	drawRightBranch();
-	
-	glPopMatrix();
 }
 
 //======================================================
@@ -627,10 +550,21 @@ void displayCallBack()
 	glLoadIdentity();
 	executeViewControl (yaw, pitch);
 	//drawAxesAndGridLines(true, true, true);
-
-	//drawM();
-	//drawC();
+	
+	glPushMatrix();
+    glTranslatef(-5.5, 0, 0);
+	drawM();
+	glPopMatrix();
+	
+	glPushMatrix();
+    glTranslatef(0, 0, 0);
+	drawC();
+	glPopMatrix();
+	
+	glPushMatrix();
+    glTranslatef(5, 0, 0);
 	drawR();
+	glPopMatrix();
 
 	glutSwapBuffers();
 }
